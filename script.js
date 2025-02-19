f7ee2e3a97ecf44c33889661ce708f4f86289a6f
Rahul01-15 <krahul2023@outlook.com>
2025-02-19 14:58:41 +0530

    js upadte

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const songs = [
   {
-    title: "Song 1",
-    author: "Artist 1",
-    src: "song1.mp3",
-    img: "cover1.jpg",
+    title: "Forest lullaby",
+    author: "lesm",
+    src: "resources/forest-lullaby-110624.mp3",
+    img: "resources/cover-1.jpg",
   },
   {
-    title: "Song 2",
+    title: "lost in the city lights",
     author: "Artist 2",
-    src: "song2.mp3",
-    img: "cover2.jpg",
+    src: "/resources/lost-in-city-lights-145038.mp3",
+    img: "resources/cover-2.jpg",
   },
 ];
 
@@ -29,43 +29,76 @@ const currentTimeEl = document.getElementById("currentTime");
 const durationEl = document.getElementById("duration");
 
 document.getElementById("play").addEventListener("click", playPause);
-document.getElementById("nextButton").addEventListener("click", nextSong);
-document.getElementById("prevButton").addEventListener("click", prevSong);
+document.getElementById("next").addEventListener("click", nextSong);
+document.getElementById("prev").addEventListener("click", prevSong);
+audio.addEventListener("progressBar", updateProgressBar);
 audio.addEventListener("timeupdate", updateProgressBar);
 
 function playPause() {
 
     if (audio.paused) {
         audio.play();
-        playIcon.src = "resources/Stop_and_play_fill-1.svg";
-        console.log("Playing audio");
+        playIcon.src = "/resources/pause-fill.svg";
+       
     } else {
         audio.pause();
-        
-        playIcon.src = "resources/Stop_and_play_fill.svg";
-        console.log("Plause audio");
+        playIcon.src = "resources/Play_fill.svg";
+       
     }
 }
 
 function nextSong() {
   // Add next button implementation
+  currentSongIndex = (currentSongIndex + 1) % songs.length;
+  loadSong(currentSongIndex);
+  audio.play();
+  playIcon.src = "/resources/pause-fill.svg";
+  
 }
 
 function prevSong() {
   // Add previous button implementation
+  currentSongIndex = (currentSongIndex - 1+songs.length) % songs.length;
+  loadSong(currentSongIndex);
+  audio.play();
+  playIcon.src = "/resources/pause-fill.svg";
+  
 }
 
 function loadSong(index) {
   // Add load song implementation
+  audio.src = songs[index].src;
+  songTitle.innerText= songs[index].title;
+  songAuthor.innerText= songs[index].author;
+  songCover.src = songs[index].img;
+  audio.pause();
+
 }
 
 function updateProgressBar() {
   // Handle when progress bar is updated
+  if (audio.duration) {
+    progressBar.value = (audio.currentTime / audio.duration) * 100;
+    currentTimeEl.innerText = formatTime(audio.currentTime);
+    durationEl.innerText = formatTime(audio.duration);
+    progressBar.style.background = `linear-gradient(to right, #C93B76 ${progressBar.value}%, #fff ${progressBar.value}%)`;
+  }
+
+    
+    
 }
 
 document.getElementById("progressBar").addEventListener("input", function () {
   audio.currentTime = (this.value / 100) * audio.duration;
+  
 });
+
+function formatTime(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = Math.floor(seconds % 60);
+  return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
+}
+
 
 // Initial load
 loadSong(currentSongIndex);
